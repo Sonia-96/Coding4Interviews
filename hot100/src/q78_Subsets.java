128b969a89aad4c72519440f18ba9bf6da7234b1
Sonia-96 <672365639@qq.com>
2021-05-16 16:16:37 +0800

    solve q78

---
+++ hot100/src/q78_Subsets.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class q78_Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int length = nums.length;
+        int n = 1 << length;
+        for (int i = 0; i < n; i++) {
+            List<Integer> subset = new ArrayList<>();
+            int index = i;
+            for (int j = 0; j < length; j++) {
+                if ((index & 1 )== 1) {
+                    subset.add(nums[j]);
+                }
+                index >>= 1;
+            }
+            res.add(subset);
+        }
+        return res;
+    }
+}
