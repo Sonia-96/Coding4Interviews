458db220aa5e6787a603f6b85f7afff6e62f7b34
Sonia-96 <672365639@qq.com>
2021-07-03 17:02:46 +0800

    solve q392

---
+++ hot100/src/q392_IsSubsequence.java
@@ -0,0 +1,12 @@
+public class q392_IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int p1 = 0, p2 = 0;
+        while (p1 < s.length() && p2 < t.length()) {
+            if (s.charAt(p1) == t.charAt(p2)) {
+                p1++;
+            }
+            p2++;
+        }
+        return p1 == s.length();
+    }
+}
